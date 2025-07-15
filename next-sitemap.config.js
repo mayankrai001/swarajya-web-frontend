409bcbb8e1c5df585178322d1c81d51ef0901721
mayankrai001 <mayankrai627@gmail.com>
2025-07-15 18:59:29 +0530

    minor improvements

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.archengineering.com",
+  siteUrl: "https://www.swarajyaconsultancy.in",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
